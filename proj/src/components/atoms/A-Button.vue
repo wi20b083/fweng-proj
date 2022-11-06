f5de479fb72591983715acc202124351399695dc
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-11-06 16:14:59 +0100

    button, link, logo

--- proj/src/components/atoms/A-Button.vue
+++ proj/src/components/atoms/A-Button.vue
@@ -1,13 +1,11 @@
 <template>
-    <div>
-        <button class="btn btn-primary">{{buttonText}}</button>
-    </div>
+    <button class="btn btn-primary">{{buttonText}}</button> 
 </template>
 
 <script>
 
 export default {
-    name: 'AtomButton',
+    name: 'A-Button',
     props:{
         buttonText: String
     }
---
+++ proj/src/components/atoms/A-Link.vue
@@ -0,0 +1,10 @@
+<template>
+    <a></a>
+</template>
+
+<script>
+
+export default{
+    
+}
+</script>

ecb384022e9cd115e9c24549b3a61d23ecc73edc
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-11-06 16:03:08 +0100

    bootstrap + button

---
+++ proj/src/components/atoms/AtomButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button class="btn btn-primary">{{buttonText}}</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AtomButton',
+    props:{
+        buttonText: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

2f560d7277fb6af215eb5f0e702d68d0f8d3e235
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-11-06 16:30:11 +0100

    nav

---
+++ proj/src/components/atoms/A-NavItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <li class="nav-item">
+        <a class="nav-link" :href="link.url">{{link.name}}</a>
+    </li>
+</template>
+
+<script>
+
+export default{
+    name: 'A-NavItem',
+    props:['link']
+}
+</script>

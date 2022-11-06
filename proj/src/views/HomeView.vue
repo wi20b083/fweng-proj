e26ae10ec04101899563b6cabba90ab505a6afc9
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-11-06 17:06:11 +0100

    router

--- proj/src/views/HomeView.vue
+++ proj/src/views/HomeView.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
-    
     <router-link to="/">Home</router-link><br>
     <router-link to="/imprint">Imprint</router-link><br>
     <router-link to="/login">Login</router-link><br>
-
-    
   </div>
 </template>
 

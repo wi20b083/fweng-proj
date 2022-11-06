bc34279abd1d20885a72743c71a09275db5ad011
wi20b083 <wi20b083@technikum-wien.at>
2022-11-06 16:25:52 +0100

    MolLabelledInput

--- proj/src/App.vue
+++ proj/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     {{ message }}
+    <MolLabelledInputVue></MolLabelledInputVue>
   </div>
 </template>
 
 <script>
+import MolLabelledInputVue from './components/molecules/MolLabelledInput.vue';
   export default {
     name: 'App',
     data () {
       return {
         message: 'Hello, Vue!'
       }
-    }
+    }, 
+    components: {MolLabelledInputVue}
 };
 </script>
---
+++ proj/src/components/atoms/AtomLabel.vue
@@ -0,0 +1,14 @@
+<template>
+    <label 
+    :for="target" 
+    class="form-label"
+    >
+    {{content}}
+    </label>
+</template>
+<script>
+export default {
+    name: 'AtomLabel', 
+    props: ['target', 'content']
+}
+</script>

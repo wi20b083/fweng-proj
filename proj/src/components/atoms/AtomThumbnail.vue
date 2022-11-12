256a672cc395e0eb8e7b23c80d708b4fac134e0c
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-11-12 17:31:40 +0100

    table

---
+++ proj/src/components/atoms/AtomThumbnail.vue
@@ -0,0 +1,10 @@
+<template>
+    <img :src="src" :alt="alt" class="img-fluid">
+</template>
+
+<script>
+export default {
+    name: 'AtomThumbnail', 
+    props: ['src', 'alt']
+}
+</script>
---
+++ proj/src/components/molecules/MolecuelProductRow.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <AtomThumbnail :src="imagesource" :alt="alttext"/>
+        </div>
+        <div class="col">
+            <AtomText :content="productname"/>
+        </div>
+        <div class="col">
+            <AtomInput inputType="number" min="0" step="1" value="1"/>
+        </div>
+        <div class="col">
+            <AtomButton content="Add" type="submit" classname="btn btn-success"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import AtomInput from '../atoms/AtomInput.vue'
+import AtomText from '../atoms/AtomText.vue'
+import AtomThumbnail from '../atoms/AtomThumbnail.vue'
+import AtomButton from '../atoms/AtomButton.vue'
+
+export default {
+    name:'MoleculeProductRow',
+    props:[
+        'productname', 'imagesource', 'alttext'
+    ],
+    components: {
+        AtomInput,
+        AtomText,
+        AtomThumbnail,
+        AtomButton
+    }
+}
+</script>

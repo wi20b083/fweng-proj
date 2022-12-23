024a304d73502662d015cb8264a1814d21bec8f1
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2022-12-23 17:26:58 +0100

    bevore rewriting project

--- proj/src/store/index.js
+++ proj/src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'vuex'
-//import userModule from './modules/userModule'
+import userModule from './modules/userModule'
 //import auctionsModule from './modules/auctionsModule'
 //import bidsModule from './modules/bidsModule'
 
